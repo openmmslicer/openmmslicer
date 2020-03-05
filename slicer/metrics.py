0a55a57e65fb2f5b0a5a2ee2700ea1d21a17d540
Miroslav Suruzhon <miroslav351@yahoo.com>
2020-03-05 16:30:08 +0000

    Addition of entropy-based resampling metrics

--- slicer/metrics.py
+++ slicer/metrics.py
@@ -41,3 +41,25 @@ class LogWorstCaseSampleSize(WorstCaseSampleSize):
     @classmethod
     def evaluate(cls, weights):
         return _np.log(super(LogWorstCaseSampleSize, cls).evaluate(weights))
+
+
+class ExpWeightEntropy(EffectiveSampleSize):
+    @classmethod
+    def defaultValue(cls, weights):
+        return 0.1
+
+    @classmethod
+    def evaluate(cls, weights):
+        weights = _np.asarray(weights)
+        weights /= _np.sum(weights)
+        return _np.exp(-_np.sum(weights * _np.log(weights)))
+
+
+class WeightEntropy(ExpWeightEntropy):
+    @classmethod
+    def defaultValue(cls, n_weights):
+        return _np.log(super(WeightEntropy, cls).defaultValue(n_weights))
+
+    @classmethod
+    def evaluate(cls, weights):
+        return _np.log(super(WeightEntropy, cls).evaluate(weights))
