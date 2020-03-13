d075096ac8cf59a0b398304dc4dc23f54edaaa40
Miroslav Suruzhon <miroslav351@yahoo.com>
2020-03-13 16:39:23 +0000

    New stable defaults for the alchemical protocol

--- slicer/integrators.py
+++ slicer/integrators.py
@@ -9,9 +9,9 @@ class AlchemicalLangevinIntegrator(_LangevinIntegrator):
     def __init__(self, *args, alchemical_functions=None, initial_lambda=0, **kwargs):
         if not alchemical_functions:
             alchemical_functions = {
-                'lambda_sterics': 'min(2*lambda, 1)',
-                'lambda_electrostatics': 'max(0, 2*lambda-1)',
-                'lambda_torsions': '1',
+                'lambda_sterics': 'min(1.25*lambda, 1)',
+                'lambda_electrostatics': 'max(0, 5*lambda-4)',
+                'lambda_torsions': 'min(1.25*lambda, 1)',
             }
 
         self._alchemical_functions = alchemical_functions
