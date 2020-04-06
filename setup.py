11b53c157af4936087a9327ea4c198a26faa42f8
Miroslav <miroslav351@yahoo.com>
2020-04-06 10:38:30 +0100

    Added setup.py, requirements.txt and a GPL license

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='slicer',
+    version='0.0.1',
+    packages=['slicer'],
+    url='',
+    license='GPL',
+    author='Miroslav Suruzhon',
+    author_email='',
+    description='Sequential LIgand Conformation ExploreR (SLICER) - A Sequential Monte Carlo Sampler for OpenMM'
+)
