a255f56375bd07a21a6322fba1c7c6a84d5a2558
Miroslav <miroslav351@yahoo.com>
2020-04-07 09:34:19 +0100

    Added a new resampling metric

--- slicer/resampling_metrics.py
+++ slicer/resampling_metrics.py
@@ -17,6 +17,17 @@ class EffectiveSampleSize:
         return _np.sum(weights) ** 2 / _np.sum(weights ** 2)
 
 
+class ExpectedSampleSize(EffectiveSampleSize):
+    @classmethod
+    def evaluate(cls, weights):
+        weights = _np.asarray(weights, dtype=_np.float32)
+        weights /= _np.sum(weights)
+        n_walkers = weights.shape[0]
+        weights *= n_walkers
+        weights = weights[weights < 1]
+        return n_walkers - (weights.shape[0] -_np.sum(weights))
+
+
 class WorstCaseSampleSize(EffectiveSampleSize):
     @classmethod
     def evaluate(cls, weights):
@@ -28,8 +39,8 @@ class WorstCaseSystematicSampleSize(EffectiveSampleSize):
     @classmethod
     def evaluate(cls, weights):
         weights = _np.asarray(weights, dtype=_np.float32)
-        int_n_particles = _np.floor(weights.shape[0] * weights)
-        return int_n_particles[int_n_particles >= 1].shape[0]
+        int_n_walkers = _np.floor(weights.shape[0] * weights)
+        return int_n_walkers[int_n_walkers >= 1].shape[0]
 
 
 class ExpWeightEntropy(EffectiveSampleSize):
